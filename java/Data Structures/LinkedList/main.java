c1797a378c82dffc1c2a411a5271e00837061565
danekweaga <danielekweaga@gmail.com>
2025-09-26 20:18:22 -0300

    Added Driver Program

--- java/Data Structures/LinkedList/main.java
+++ java/Data Structures/LinkedList/main.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
  * Driver program for LinkedList class
  * @author Chukwunonso Ekweaga
  */
-public class LinkedListDriver
+public class main
  {
     public static void main(String[] args)
      {
